677c0abadf6e5df1c1c9e90bea86b7e72a68f4cd
Zsolt Harmath <harmathzs@gmail.com>
2026-01-22 11:06:27 +0100

    Remove array notation

--- react-app/netlify/functions/fruits.js
+++ react-app/netlify/functions/fruits.js
@@ -4,7 +4,7 @@ export const handler = async (event, context) => {
     //console.log('endpoint /fruits called', event, context)
 
     const sql = neon(); // automatically uses env NETLIFY_DATABASE_URL
-    const [fruits] = await sql`SELECT * FROM fruits LIMIT 50`;
+    const fruits = await sql`SELECT * FROM fruits LIMIT 50`;
     console.log('fruits', fruits)
 
     if (event.httpMethod == 'GET') {
